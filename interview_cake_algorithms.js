b48ffc6e351633679f75a6e513d26ade1c2b4d6b
Malek <mmalek1421@gmail.com>
2017-01-29 17:19:08 -0500

    added product except at integer

--- interview_cake_algorithms.js
+++ interview_cake_algorithms.js
@@ -1,7 +1,7 @@
 // Write an efficient function that takes stockPricesYesterday and
 // returns the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
 
-function myFunction(stockPricesYesterday) {
+function findProfit(stockPricesYesterday) {
     var min = Math.min(...stockPricesYesterday)
     var start = stockPricesYesterday.indexOf(min);
     var profits = [];
@@ -11,4 +11,19 @@ function myFunction(stockPricesYesterday) {
         }
     }
     return Math.max(...profits);
+}
+
+// You have an array of integers, and for each index you want to find the product of every integer
+// except the integer at that index.
+
+function productExceptIndex(arr) {
+	var products = [];
+    for (var i = 0; i < arr.length; i++) {
+        var arrCopy = arr;
+        var current = arr[i]
+    	arrCopy.splice(i,1);
+        products.push(arrCopy.reduce((a,b) => a * b));
+        arrCopy.splice(i, 0, current);
+    }
+   	return products;
 }
